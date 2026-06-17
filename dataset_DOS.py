780d802eeb6e8505e464bc0d980530086d38a0d6
fquintavalle <franciquinta01@gmail.com>
2026-06-17 20:35:37 +0000

    Initial commit

---
+++ dataset_DOS.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+def epsilon_3D(kx,ky,kz):
+    en = -0.5*(np.cos(kx)+np.cos(ky)+np.cos(kz))
+
+    return en
+
+def dos(E, epsilon, de):
+    dos = np.zeros(len(E))
+
+    for i, Ei in enumerate(E):
+        diff = Ei - epsilon
+        dos[i] = np.sum(de / (diff**2 + de**2))
+
+    dos = dos / len(epsilon)
+    dos = dos / np.trapz(dos, E)
+
+    return dos
+
+
+Ne = 100
+de = 1e-2
+kx = np.linspace(-np.pi,np.pi,Ne)
+ky = kx
+kz = kx
+X,Y,Z = np.meshgrid(kx,ky,kz)
+
+e = epsilon_3D(X,Y,Z).ravel()
+e_min = np.min(e)
+e_max = np.max(e)
+
+E_grid = np.linspace(e_min, e_max, 1000)
+de_values = np.linspace(0.005, 0.1, 50)
+
+A = []
+B = []
+
+for de in de_values:
+    dos_values = dos(E_grid, e, de)
+
+    for E, rho in zip(E_grid, dos_values):
+        A.append([E, de])
+        B.append([rho])
+
+A = np.array(A)
+B = np.array(B)
+
+
+np.savez_compressed(
+    "dos_dataset.npz",
+    A=A,
+    B=B,
+    e=e,
+    E_grid=E_grid,
+    de_values=de_values
+)
+
