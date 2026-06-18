c9d5571bb80080a6c29fef451006fa8555c42cbd
fquintavalle <franciquinta01@gmail.com>
2026-06-18 09:42:42 +0000

    First bug fix

--- dataset_DOS.py
+++ dataset_DOS.py
@@ -36,41 +36,42 @@ def dos_parallel(E_grid, epsilon, de, n_jobs=15):
 
     return rho
 
+if __name__ == "__main__":
 
-Ne = 200
-de = 1e-2
-kx = np.linspace(-np.pi,np.pi,Ne)
-ky = kx
-kz = kx
-X,Y,Z = np.meshgrid(kx,ky,kz)
+    Ne = 200
+    de = 1e-2
+    kx = np.linspace(-np.pi,np.pi,Ne)
+    ky = kx
+    kz = kx
+    X,Y,Z = np.meshgrid(kx,ky,kz)
 
-e = epsilon_3D(X,Y,Z).ravel()
-e_min = np.min(e)
-e_max = np.max(e)
+    e = epsilon_3D(X,Y,Z).ravel()
+    e_min = np.min(e)
+    e_max = np.max(e)
 
-E_grid = np.linspace(e_min, e_max, 1000)
-de_values = np.linspace(0.005, 0.1, 50)
+    E_grid = np.linspace(e_min, e_max, 1000)
+    de_values = np.linspace(0.005, 0.1, 50)
 
-A = []
-B = []
+    A = []
+    B = []
 
-for de in de_values:
-    dos_values = dos_parallel(E_grid, e, de)
+    for de in de_values:
+        dos_values = dos_parallel(E_grid, e, de)
 
-    for E, rho in zip(E_grid, dos_values):
-        A.append([E, de])
-        B.append([rho])
+        for E, rho in zip(E_grid, dos_values):
+            A.append([E, de])
+            B.append([rho])
 
-A = np.array(A)
-B = np.array(B)
+    A = np.array(A)
+    B = np.array(B)
 
 
-np.savez_compressed(
-    "dos_dataset.npz",
-    A=A,
-    B=B,
-    e=e,
-    E_grid=E_grid,
-    de_values=de_values
-)
+    np.savez_compressed(
+        "dos_dataset.npz",
+        A=A,
+        B=B,
+        e=e,
+        E_grid=E_grid,
+        de_values=de_values
+    )
 
